554f5f2826d857cb333bd76cfd0736188b0b4cde
Saroj Adhikari <sarojadh1111@gmail.com>
2025-06-21 14:40:13 +0545

    learn mkdir

--- fileHandling.js
+++ fileHandling.js
@@ -1,4 +1,4 @@
-import {readFile, writeFile , appendFile} from 'fs/promises'
+import {readFile, writeFile , appendFile,mkdir} from 'fs/promises'
 
 
 // README FILE : THIS above line of code help to read text from text.jsx file .
@@ -8,16 +8,30 @@ const readeFile = async (filename)=>{
 
 };
 
-
+// this line of code write in a file . 
 const write_file =async (filename, content)=>{
 await writeFile(filename,content);
 };
 write_file("ai.py","This is a python file .");
 
 readeFile("ai.py");
-
+// this line of code add text inside ai.py .
 const appendfile = async(filename,content)=>{
     await appendFile(filename,content);
     console.log("secessfully added ");
 }
+// this line code write text inside ai.py .
 appendfile("ai.py","added by appendfile . ")
+
+// this line of code only generate one file 
+const file_dir =async (dir)=>{
+    await mkdir (dir);
+
+};
+file_dir("page")
+
+// this line of code make a file inside home i.e : first ,second .
+const dir= async(path)=>{
+    await mkdir (path, {recursive:true})
+}
+dir("home/first/second")
