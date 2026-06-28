aedbeb8310288dfb9c5559dfed6c10110ef8436a
Sarojadhi <sarojadh1111@gmail.com>
2026-06-28 12:30:58 +0545

    :3

--- ErrorHandling/app.js
+++ ErrorHandling/app.js
@@ -1,11 +1,12 @@
 const http = require("http");
-const errorHandler = require("./error");
-
+// const errorHandler = require('./error');
+const  runtimeErrorHandler= require('./runtimeError')
 const server = http.createServer((req, res) => {
   console.log(req.url, req.method);
 
-  console.log(errorHandler);
-
+  // console.log(errorHandler);
+  console.log(runtimeErrorHandler);
+  runtimeErrorHandler();
   res.end("Server is running");
 });
 
@@ -13,4 +14,4 @@ const PORT = 3000;
 
 server.listen(PORT, () => {
   console.log(`Server Running at http://localhost:${PORT}`);
-});
+}); 
---
+++ ErrorHandling/runtimeError.js
@@ -0,0 +1,6 @@
+const runtimeErrorHandler = () => {
+  // const a = 30;
+  // a(); 
+// this a will make a run time error . 
+};
+module.exports = runtimeErrorHandler; 
